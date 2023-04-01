b4f5cbd90d8504a3790f58793543f908759288a5
LombardiniJere <jere.lombardini@gmail.com>
2023-03-31 22:36:17 -0300

    htmlUpdate2

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 
+const { src, dest, watch } = require('gulp');
 
-const {src, dest } = require("gulp");
+// CSS
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
-
-function css(done) {
-    src("src/scss/app.scss")
-    .pipe(sass())
-    .pipe(dest("build/css"));
+function css( done ) {
+    src('src/scss/**/*.scss') // Identificar el archivo .SCSS a compilar
+        .pipe( plumber())
+        .pipe( sass() ) // Compilarlo
+        .pipe( dest('build/css') ) // Almacenarla en el disco duro
     done();
 }
 
+function javascript( done ) {
+    src('src/js/**/*.js')
+        .pipe(dest('build/js'));
 
+    done();
+}
 
+function dev( done ) {
+    watch('src/scss/**/*.scss', css);
+    done();
+}
 
-
+ 
 exports.css = css;
+exports.dev = dev;
+exports.js = javascript;
